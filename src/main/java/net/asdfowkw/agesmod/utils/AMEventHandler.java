cc344a16cc716bcf530a04374818dfd0908e75d2
Erica <palosaari.erica@gmail.com>
2017-03-08 19:05:43 +0100

    Useless Tools

--- src/main/java/net/asdfowkw/agesmod/utils/AMEventHandler.java
+++ src/main/java/net/asdfowkw/agesmod/utils/AMEventHandler.java
@@ -1,18 +1,49 @@
 package net.asdfowkw.agesmod.utils;
 
-import net.minecraft.block.Block;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
-import net.minecraftforge.event.world.BlockEvent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class AMEventHandler {
 
-    @SubscribeEvent
-    public void onBreakBlock(BlockEvent.BreakEvent event, Block block, EntityPlayer player, int meta) {
-        if(player.getHeldEquipment() == Items.IRON_PICKAXE) {
+    /*@SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void breakBlock(BlockEvent.BreakEvent event) {
+        if(FMLCommonHandler.instance().getEffectiveSide().isClient())
+            return;
+
+        EntityPlayer player = event.getPlayer();
+
+        if(player.inventory.getCurrentItem().getItem() != Items.IRON_PICKAXE) {
             event.setCanceled(true);
         }
+    }*/
+
+    @SubscribeEvent
+    public void breakSpeed(PlayerEvent.BreakSpeed event)
+    {
+        if(event.getEntityPlayer() == null)
+            return;
+        ItemStack itemStack = event.getEntityPlayer().inventory.getCurrentItem();
+        if(itemStack == null)
+            return;
+        if(isUselessTool(itemStack.getItem()))
+            event.setNewSpeed(0);
+    }
+
+    public static boolean isUselessTool(Item item) {
+
+        if(item == null)
+            return false;
+
+        //if(IguanaTweaks.toolWhitelist.contains(item))
+        //    return false;
+
+        if(item instanceof ItemTool)
+            return true;
+
+        return false;
     }
 
 }
